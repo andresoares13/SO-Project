5371dd0670d1ff2ffd239c2d681ff00de31d0867
Andre Soares <andre.soarestc13@gmail.com>
2022-05-21 21:27:47 +0100

    completes q2

--- q2/addmx.c
+++ q2/addmx.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]){
   if (argc<3  || argc>4 || (argc==3 && (!strstr(argv[1],".txt")||!strstr(argv[1],".txt")))){
@@ -71,33 +72,100 @@ int main(int argc, char* argv[]){
     perror("mmap");
     exit(EXIT_FAILURE);
   }
-  for(int i = 0; i < nProcesses; i++){
+  for(int i = 0; i < memsize; i++){
       partials[i] = 0;
   }
 
-  for(i = 0; i < nProcesses; i++) {
+  rewind(ptr1);
+  for(int i = 0; i < nProcesses; i++) {
     pid_t pid;
     if ((pid = fork()) < 0) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
     if(pid == 0) {
-        for(int j = 0; j < n; j++){
-            if(j % nProcesses == i){
-                for(int k = 0; k < n; k++){
-                    if(matrix[j][k] > threshold){
-                        partials[i]++;
-                        exit(EXIT_SUCCESS);
-                    }
-                }
+      
+      for (int j=0;j<nCollumns;j++){
+        int x,y;
+        int counter = j;
+        int emptySkips = i; //if i=0 you dont wanna skip to the 3rd character, if the i=1 you wanna get to the column 3rd character and so on
+        bool skipLine=true;
+        while(!feof(ptr1)){
+          ch= fgetc(ptr1);
+          if (ch!=10&&skipLine){ // while characters are different than new line but we want to skip that line, we skip
+            continue;
+          }
+          else{
+            if (ch==10){ //when it is the new line, we want to not skip line anymore if we already skipped the ammount of lines we needed to (varies with j)
+              if (counter==0){
+                skipLine=false;
+                counter=j;
+                continue;
+              }
+              counter--;
+              continue;
             }
+            if (emptySkips==0 && ch!=32){ //varies with i, if we are in the line we want, we need to make sure to acess the right character, basically its the amount of times we skip the empy char separating the columns and it is due to the process number
+              x=ch - '0';
+              break;
+            }
+            if (!skipLine && ch==32){
+              emptySkips--;
+            }
+          }
         }
-            
-                
-                    
-                        
-                    
+        emptySkips=i;
+        skipLine=true;
+        while(!feof(ptr2)){
+          ch= fgetc(ptr2);
+          if (ch!=10&&skipLine){
+            continue;
+          }
+          else{
+            if (ch==10){
+              if (counter==0){
+                skipLine=false;
+                counter=j;
+                continue;
+              }
+              counter--;
+              continue;
+            }
+            if (emptySkips==0 && ch!=32){
+              y=ch - '0';
+              break;
+            }
+            if (!skipLine && ch==32){
+              emptySkips--;
+            }
+          }
+        }
+        
+        rewind(ptr1); //goes back to the beggining of both files before starting next process
+        rewind(ptr2);
+        partials[(i*nCollumns)+j]=x+y; // store in the shared space the sum of both characters in the line and column corresponding to the process, process 1 takes care of 4 lines in the 1st column in the example
+      }
+      exit(EXIT_SUCCESS);
+      
+    }        
+  }
 
+  for(int i = 0; i < nProcesses; i++) { //waits for the processes to end
+    if ( waitpid(-1, NULL, 0) < 0) {
+      perror("waitpid");
+      exit(EXIT_FAILURE);
+    }
+  }
   
+  
+  printf("%d%s%d\n",nCollumns,"x",nProcesses); //prints out the result in the correct format
+  for (int j=0;j<nCollumns;j++){
+    for (int i =0; i<nProcesses;i++){
+      printf("%d ",partials[j+(nCollumns*i)]);
+    }
+    printf("\n");
+  }          
+            
+    
   return 0;
 }
